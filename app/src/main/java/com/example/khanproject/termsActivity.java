88f6210a7829d54241c5eb8949111071510bde0c
nadiamalik7077 <nadiamalik.7077@gmail.com>
2023-12-14 00:54:22 -0800

    Initial commit

---
+++ app/src/main/java/com/example/khanproject/termsActivity.java
@@ -0,0 +1,14 @@
+package com.example.khanproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class termsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+    }
+}
